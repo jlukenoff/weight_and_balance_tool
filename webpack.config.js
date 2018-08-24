9698c02952c4744773a63a1b045d0f7b4678d82f
John Lukenoff <jlukenoff@gmail.com>
2018-08-24 15:02:22 -0700

    (init)

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+
+const ENTRY_POINT = path.resolve(__dirname, 'client/index.jsx');
+
+const OUTPUT_DIR = path.resolve(__dirname, 'public/dist');
+
+module.exports = {
+  entry: ENTRY_POINT,
+  output: {
+    filename: 'bundle.js',
+    path: OUTPUT_DIR,
+  },
+  devtool: 'source-map',
+  resolve: { extensions: ['.js', '.jsx'] },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        loader: 'style-loader',
+      },
+      {
+        test: /\.css$/,
+        loader: 'css-loader',
+        query: {
+          modules: true,
+          localIdentName: '[name]__[local]___[hash:base64:5]',
+        },
+      },
+      {
+        test: /.jsx?/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['env', 'react', 'airbnb'],
+        },
+      },
+    ],
+  },
+};
